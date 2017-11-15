221b2f2c9e48f48413bd3dd36537e3bb4ac6ee7a
TuanPham303 <phamcongtuan303@gmail.com>
2017-11-15 10:12:41 -0800

    fix top image and hamburger icon to be responsive

--- header.php
+++ header.php
@@ -58,15 +58,14 @@
 
 				
 			<div class="header-container">
-					<?php $about_page = get_page_by_title( 'about us');?>
-					<div class="heade-image"><img src="<?php the_field('about-image',$about_page); ?>"/></div>
+					<?php $about_page = get_page_by_title('about us');?>
 					<div class="mobile-header">
 						<div class="dot-dot-dot-image">
 							<img src="<?php echo get_stylesheet_directory_uri(); ?>/image/PSP_logo-mobile-08.svg" width="65%" height="80">	
 						</div>
-						<span id="hamburger" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+						<div id="hamburger" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</div>
 					</div>
-					
+					<div class="heade-image"><img src="<?php the_field('about-image',$about_page); ?>"/></div>
 			</div>
 		  	
 
